a2b16ecd679152fedd134b9ab3aecdb856fba18c
devs35 <don.vu.22@gmail.com>
2017-09-06 23:00:36 -0400

    First commit

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,12 +10,27 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
-
+mongoose.connect('mongodb://user:password@ds125994.mlab.com:25994/devs35');
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
+fs.readFile('listings.json', 'utf8', function(err, data) {
+    JSON.parse(data).entries.forEach(function (lst) {
+        var list = new Listing({
+            code: lst.code,
+            name: lst.name,
+            coordinates: {
+                latitude: lst.latitude,
+                longitude: lst.longitude
+            },
+            address: lst.address
+        });
+        list.save(function (err) {
+            if (err) {throw err;};
+        });
+    });
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- queries.js
+++ queries.js
@@ -1,10 +1,20 @@
 /* Fill out these functions using Mongoose queries*/
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
 
+mongoose.connect('mongodb://user:password@ds125994.mlab.com:25994/devs35');
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({ name: 'Library West' }, function(err, data) {
+      if (err) {throw err;};
+      console.log(data);
+  })
 };
 var removeCable = function() {
   /*
@@ -12,17 +22,29 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({ code: 'CABL'}, function (err, data) {
+     if (err) {throw err;};
+     console.log(data);
+  });
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({ code: 'PHL' }, { address: 'New Address' }, function (err, data) {
+      if (err) {throw err;};
+      console.log(data);
+  });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find({}, function(err, data) {
+     if (err) {throw err;};
+     console.log(data);
+  });
 };
 
 findLibraryWest();
